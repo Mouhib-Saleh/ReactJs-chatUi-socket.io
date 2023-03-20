9dd6f0e7c703e6cc736983ff2ed07f0bc0e707fe
mouhib saleh <mouhib.saleh20@gmail.com>
2023-03-20 16:29:18 +0100

    fixed theme

--- src/component/ChatWidget.jsx
+++ src/component/ChatWidget.jsx
@@ -13,7 +13,18 @@ const ChatWidget = (props) => {
 
   return (  
     <div>
-      
+        <Chat isVisible={showDiv} ChatApi={props.ChatApi}/>     
+        <div className="button-container">
+        <button className="message-button"  onClick={handleClickShow}>
+           <ChatIcon/>
+        </button>
+     </div>
+     <Chat isVisible={showDiv} ChatApi={props.ChatApi}/>     
+        <div className="button-container">
+        <button className="message-button"  onClick={handleClickShow}>
+           <ChatIcon/>
+        </button>
+     </div>
     </div>
   );
 };
